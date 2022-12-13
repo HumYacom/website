8c6adcfa73ca1dc5a51617268f773d35a677b5a4
HumYacom <thainakdee06@hotmail.com>
2022-12-13 16:23:31 +0700

    new2

--- run/app.py
+++ run/app.py
@@ -10,14 +10,17 @@ def index():
 def work():
     return render_template("work.html")
 
-@app.route("/sendData")
+@app.route("/return")
 def adminup():
     idname = request.args.get("idname")
-    level = request.args.get("level")
-    department = request.args.get("department")
-    sos = request.args.get("sos")
-    notify = request.args.get("notify")
-    return render_template("youfuck.html",data={"name":idname ,"level":level,"department":department,"sos":sos,"notify":notify})
+    sale = request.args.get("sale")
+    mail = request.args.get("mail")
+    price = request.args.get("price")
+    tax = request.args.get("tax")
+    Payment = request.args.get("Payment")
+    tel = request.args.get("tel")
+    addr = request.args.get("addr")
+    return render_template("su.html",data={"name":idname ,"sale":sale,"mail":mail,"price":price,"tax":tax,"Payment":Payment,"tel":tel,"addr":addr})
 
 if __name__ == "__main__":
     app.run(debug=True)
