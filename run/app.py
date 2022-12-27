e7c5c47307741bcfeb579b34dfd69203e54f11b3
HumYacom <thainakdee06@hotmail.com>
2022-12-27 16:45:29 +0700

    new v1.8

--- run/app.py
+++ run/app.py
@@ -1,15 +1,26 @@
 from flask import Flask,render_template,request
+import pymysql
+
+db = pymysql.connect(host="localhost",user="root",passwd="",database="table")
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    data = {"name":"menu","adddoc":"AddDoc"}
-    return render_template("index.html",mydata = data)
+def show():
+    with db:
+        cur = db.cursor()
+        sql = "SELECT * FROM list_products"
+        cur.execute(sql)
+        rows = cur.fetchall()
+        print(rows)
+        return "ok"
+   # return render_template("index.html",mydata = data)
 
 @app.route("/work") 
 def work():
     return render_template("work.html")
+
+
 
 
 
